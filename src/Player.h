b8ce4a355db90f478dbc3ecc188b71340f43e9ec
Ivyson Lucas <ivysonlucas27@hotmail.com>
2025-08-21 23:35:56 -0300

    feat: function to draw and to move with properly normalized vectors

--- src/Player.h
+++ src/Player.h
@@ -3,16 +3,20 @@
 
 #include <iostream>
 #include <raylib.h>
+#include <math.h>
 
 class Player {
   private:
     Vector2 pos;
     float speed;
+    Color color;
 
   public:
-    Player(Vector2 startPos) : pos(startPos) {}
-
-    void move() {}
-};
+    Player(Vector2 startPos, float startSpeed, Color color) : pos(startPos), speed(startSpeed), color(color) {}
+  
+    void setSpeed(float speed);
+    void move(float deltaX, float deltaY);
+    void draw();
+  };
 
 #endif
